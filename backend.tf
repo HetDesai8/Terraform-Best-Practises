0107affceec1448ae63e494ce21ceb4b94b9ecb7
HetDesai8 <77717443+HetDesai8@users.noreply.github.com>
2023-06-25 03:25:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
-# terraform {
+terraform {
 
-#   backend "s3" {
+  backend "s3" {
 
-#     bucket = "het-remote-bucket"
-#     key    = "AWS_EVALUATION/terraform.tfstate"
-#     region = "us-east-1"
-#     dynamodb_table = "het-remote-dynamodb"
+    bucket = "het-remote-bucket"
+    key    = "AWS_EVALUATION/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "het-remote-dynamodb"
 
-#   }
+  }
 
-# }
+}
